66c91a9e9169315a8218758f27c409dd817b1a41
Stephen Abrokwah <AbrokwahS@gmail.com>
2021-06-14 13:40:30 -0400

    intial commit

---
+++ db/migrations/20210614124458_user_migration.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class UserMigration extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change(): void
+    {
+        $users = $this->table('users');
+        $users->addColumn('first_name', 'string', ['limit' => 20])
+              ->addColumn('last_name', 'string', ['limit' => 40])
+              ->addColumn('address1', 'string', ['limit' => 191])
+              ->addColumn('address2', 'string', ['limit' => 100])
+              ->addColumn('city', 'string', ['limit' => 30])
+              ->addColumn('state', 'string', ['limit' => 191])
+              ->addColumn('zip', 'string', ['limit' => 9])
+              ->addColumn('country', 'string', ['limit' => 191,'default' => 'US'])
+              ->addColumn('created', 'datetime')
+              ->addColumn('updated', 'datetime', ['null' => true])
+              ->create();
+    }
+}
---
+++ db/seeds/UserSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+
+use Phinx\Seed\AbstractSeed;
+
+class UserSeeder extends AbstractSeed
+{
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeders is available here:
+     * https://book.cakephp.org/phinx/0/en/seeding.html
+     */
+    public function run()
+    {
+
+        $faker = Faker\Factory::create();
+        $data = [];
+        for ($i = 0; $i < 10; $i++) {
+
+            //randomaly generate if the zip is going to be 9 or 5 digits
+            $zip = '';
+            if(rand(1,2) == 1)
+            {
+                $zip = rand(11111,99999);
+            }
+            else
+            {
+                $zip = rand(111111111,999999999);
+            }
+
+            $data[] = [
+                'first_name'    => $faker->firstName() ,
+                'last_name'    => $faker->lastName(),
+                'address1'    => $faker->streetAddress(),
+                'address2'    => '',
+                'city'    => $faker->city(),
+                'state'    => $faker->state(),
+                'zip'    => $zip,
+                'created' => date('Y-m-d H:i:s'),
+                'updated' => NULL,
+            ];
+        }
+        $this->table('users')->insert($data)->saveData();
+    }
+}
